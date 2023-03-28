49c95625816c7c1ab6de275cd03d4d48df1723c3
Gastón Gorosito <gorosito.gaston@gmail.com>
2023-03-28 19:25:56 -0300

    primeras correciones

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,13 @@
+import "./index.css";
+
+
+const Navbar = () => {
+  return <div className="Navbar">
+  <a class="dropdown-item" href="#">Inicio</a>
+  <a class="dropdown-item" href="#">Shop</a>
+  <a class="dropdown-item" href="#">contacto</a></div>;
+}
+  
+
+
+export default Navbar;
