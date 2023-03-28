1e61e6b95fc1947cc974d7556d39531d2f04d165
Gastón Gorosito <gorosito.gaston@gmail.com>
2023-03-28 20:08:40 -0300

    correccion nav

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -4,11 +4,12 @@ import CartWidget from "../CartWidget/CartWidget";
 
 const Navbar = () => {
   return  <div className="Navbar">
-  <a class="dropdown-item" href="#">Inicio</a>
-  <a class="dropdown-item" href="#">Shop</a>
-  <a class="dropdown-item" href="#">contacto</a></div>;
+    <nav>
+        <a class="dropdown-item" href="#">Inicio</a>
+        <a class="dropdown-item" href="#">Shop</a>
+      <a class="dropdown-item" href="#">Contacto</a>
+    </nav>
+  </div>
 }
-
-
 
 export default Navbar;
