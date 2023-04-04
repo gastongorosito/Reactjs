3444a1fbd70f0b2a4a4ebcb7706678e6dbfab099
Gastón Gorosito <gorosito.gaston@gmail.com>
2023-04-04 18:47:33 -0300

    ItemListContainer

---
+++ src/components/ItemListContainer/itemlistcontainer.js
@@ -0,0 +1,11 @@
+
+const ItemListContainer = () => {
+  return (
+    <section className="item-list-container">
+      <h2 className="item-list-container__title">Productos</h2>
+      <ItemList />
+    </section>
+  );
+};
+
+export default ItemListContainer;
